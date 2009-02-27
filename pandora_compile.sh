d7874b819f7d8115c6e166e5a873ab6ab54d7e13
cpasjuste <cpasjuste@cpasjuste-laptop.(none)>
2009-02-27 16:29:00 +0100

    updated pandora_compile.sh script

--- pandora_compile.sh
+++ pandora_compile.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-./configure --host=arm-none-linux-gnueabi --target=arm-none-linux-gnueabi --enable-pandora
+autoreconf
+make clean
+./configure --host=arm-none-linux-gnueabi --target=arm-none-linux-gnueabi --enable-pandora --with-png=$PNDSDK/usr --with-sdl-prefix=$PNDSDK/usr
 make
